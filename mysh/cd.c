44da0838de01741d3ffec500cb4b266b81c0de3f
Veronika Levkevich <veronika.levkevich@gmail.com>
2011-12-02 19:00:16 +0300

    Standart error reporting with perror.

--- mysh/cd.c
+++ mysh/cd.c
@@ -13,35 +13,10 @@ int cd(const char* path)
   }
 
   if (chdir(path) < 0) {
-
-    switch (errno) {
-    case EACCES:
-      printf("Permission is denied\n");
-      break;
-    case EFAULT:
-      printf("Path points outside your accessible address space\n");
-      break;
-    case EIO:
-      printf("An I/O error occurred\n");
-      break;
-    case ENAMETOOLONG:
-      printf("Path is too long\n");
-      break; 
-    case ENOENT:
-      printf("No such directory\n");
-      break;
-    case ENOMEM:
-      printf("Insufficient kernel memory was available\n");
-      break;
-    case ENOTDIR:
-      printf("Not a directory\n");
-      break;
-    default:
-      printf("Unknown error\n");
-    }
+    perror(NULL);
     return 1;
-
   } 
+
   return 0;
 }
 
@@ -49,17 +24,7 @@ int pwd()
 {
   char * cwd = getcwd(NULL, 0);
   if (NULL == cwd) {
-
-    switch (errno) {
-    case EACCES:
-      printf("Permission is denied\n");
-      break;
-    case ENOENT:
-      printf("The current working directory has been unlinked\n");
-      break;
-    default:
-      printf("Unknown error\n");
-    }
+    perror(NULL);
     return 1;
 
   } else {
